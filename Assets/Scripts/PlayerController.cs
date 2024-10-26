9ccb4a6f53c2e153081151cfa27104df7da6fe8f
sherdst <102920341+sherdst@users.noreply.github.com>
2024-10-27 00:57:31 +0300

    adam ettim
    a

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    public float speed = 5f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float X = Input.GetAxisRaw("Horizontal");
+        float Y = Input.GetAxisRaw("Vertical");
+
+        Vector2 movement = new Vector2(X, Y);
+        movement.Normalize();
+
+        rb.velocity = movement * speed;
+
+        if(movement != movement * speed)
+        {
+            float angle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+
+
+    }
+}
