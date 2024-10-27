a7c79ce671a2dac211523a337f868f5a9ffafd37
Semih Tunçer <semih.tuncher@gmail.com>
2024-10-27 12:25:47 +0300

    daha havalı movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,28 +8,35 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D rb;
 
     public float speed = 5f;
+    public float rotationSpeed;
+
+    float x;
+    float y;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+    }
+
     private void FixedUpdate()
     {
-        float X = Input.GetAxisRaw("Horizontal");
-        float Y = Input.GetAxisRaw("Vertical");
+        Vector2 movement = new Vector2(x, y);
 
-        Vector2 movement = new Vector2(X, Y);
-        movement.Normalize();
+        if (Math.Abs(x) == 1 && Math.Abs(y) == 1)
+            movement /= (float)Math.Sqrt(2);
 
         rb.velocity = movement * speed;
 
-        if(movement != movement * speed)
+        if (x != 0 || y != 0)
         {
             float angle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0, 0, angle);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, angle), Time.fixedDeltaTime * rotationSpeed);
         }
-
-
     }
 }
